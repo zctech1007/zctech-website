6e4ec65b1d4309b73b62136b795bec703f5a654e
João Pedro Zabarella <zabarellaj@gmail.com>
2025-10-01 00:50:25 -0300

    fix(router): Adiciona 'basename' ao BrowserRouter para deploy no Pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+<QueryClientProvider client={queryClient}>
+<TooltipProvider>
+<Toaster />
+<Sonner />
+ <BrowserRouter basename="/zctech-website/">
+ <Routes>
+ <Route path="/" element={<Index />} />
+
+ <Route path="*" element={<NotFound />} />
+ </Routes>
+ </BrowserRouter>
+ </TooltipProvider>
+ </QueryClientProvider>
 );
 
 export default App;
